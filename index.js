fbce929f7eb54325b8f47ae45cc64f6641a74808
Joshua Allford <joshallfordnanook@gmail.com>
2021-08-18 15:43:14 +0100

    Assignment Complete!

---
+++ index.js
@@ -0,0 +1,82 @@
+function add(a, b) {
+
+
+
+    return (a + b)
+
+
+
+
+}
+
+function subtract(a, b) {
+
+    return (a - b)
+
+}
+
+function multiply(a, b) {
+
+    return (a * b)
+
+}
+
+function divide(a, b) {
+
+    return (a / b)
+
+}
+
+add()
+
+subtract()
+
+multiply()
+
+divide()
+
+function increment(a) {
+
+
+
+    a++;
+
+    return a;
+
+}
+
+increment()
+
+function decrement(n) {
+
+
+
+    n--;
+
+    return n;
+
+}
+
+decrement()
+
+function makeInt(a) {
+
+
+    return parseInt(a, 10);
+
+
+
+}
+
+makeInt()
+
+function preserveDecimal(b){
+
+    return parseFloat(b)
+}
+
+preserveDecimal()
+
+
+
+
